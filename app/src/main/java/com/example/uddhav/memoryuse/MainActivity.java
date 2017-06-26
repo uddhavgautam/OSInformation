b4c54c3cfd488da5fa9ff74633af3c8ec8de0cae
uddhav <upgautam@ualr.edu>
2017-06-26 17:11:10 -0500

    all done

--- app/src/main/java/com/example/uddhav/memoryuse/MainActivity.java
+++ app/src/main/java/com/example/uddhav/memoryuse/MainActivity.java
@@ -32,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
                     e.printStackTrace();
 
                 }
+
                 startProcessing();
             }
         });
